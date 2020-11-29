e769a05d77d57493e4a8927037a102fdb186bc65
Pierre Santos <santospierre1305@gmail.com>
2020-11-29 17:12:48 +0100

    Adding loading spinner between filter change

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -1,4 +1,4 @@
-<div x-data class="md:max-w-screen-md md:mx-auto" wire:init='loadProducts'>
+<div x-data class="md:max-w-screen-md md:mx-auto" >
     <div>
         {{-- Header --}}
         <div class="fixed top-0 z-10 w-full pt-5 dark:bg-blueGray-900 bg-coolGray-100 md:max-w-screen-md">
@@ -12,7 +12,7 @@
                 <div class="flex-no-wrap w-full mx-5 space-x-2 overflow-x-scroll overflow-y-hidden" style="display: -webkit-box">
                     <div
                         wire:click="sortBy('')"
-                        class="block px-4 py-1 text-sm font-medium rounded-full text-blueGray-100 dark:bg-blueGray-600 bg-blueGray-400"
+                        class="block px-4 py-1 text-sm font-medium transition duration-150 ease-in rounded-full dark:transition dark:duration-150 dark:ease-in text-blueGray-100 dark:bg-blueGray-600 bg-blueGray-400"
                         :class="{'opacity-50' : $wire.filter !== '' }"
                         >
                         <span>All</span>
@@ -32,10 +32,19 @@
         {{-- Menu --}}
         <div class="mt-32 md:mt-40">
             {{-- Container --}}
-            <div class="mx-5 space-y-4">
+            {{-- Loading when change filter --}}
+            <div class="flex items-center justify-center">
+                <div wire:loading wire:target='sortBy'>
+                    <svg class="w-5 h-5 mr-3 -ml-1 text-blueGray-600 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                </div>
+            </div>
+            <div class="mx-5 mb-5 space-y-4" wire:init='loadProducts' wire:loading.remove wire:target='sortBy'>
                 {{-- Product --}}
                 @forelse($products as $key => $product)
-                    <x-product-tile :product='$product' />
+                    <x-product-tile :product='$product'/>
                 @empty
                     {{-- Loadin State --}}
                     @foreach (range(1,5) as $game)
